30a23cc1681e68c8b539bb009b5164df5e46171c
Hung Vu <vunamhung@gmail.com>
2022-01-09 23:45:58 +0700

    client env

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,7 @@
 import { hydrate } from 'react-dom';
 import { RemixBrowser } from 'remix';
 
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && window.ENV.NODE_ENV !== 'development') {
   // we will register it after the page complete the load
   window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'));
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,21 @@
 import type { MetaFunction, LinksFunction } from 'remix';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from 'remix';
 import stylesUrl from './style.css';
 
 export const meta: MetaFunction = () => ({ title: 'New Remix App' });
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesUrl }];
 
+export function loader() {
+  return {
+    ENV: {
+      NODE_ENV: process.env.NODE_ENV,
+    },
+  };
+}
+
 export default function App() {
+  const data = useLoaderData();
+
   return (
     <html lang="en">
       <head>
@@ -19,6 +29,7 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === 'development' && <LiveReload />}
+        <script dangerouslySetInnerHTML={{ __html: `window.ENV = ${JSON.stringify(data.ENV)}` }} />
       </body>
     </html>
   );
