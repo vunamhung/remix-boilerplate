1ce6dfa2e148d5bb2b9223eab058c036fedccbb2
Hung Vu <vunamhung@gmail.com>
2023-08-01 12:21:33 +0700

    🔥 chore(useRevalidate.ts): remove unused useRevalidate hook
    The useRevalidate hook was no longer being used in the application and was causing unnecessary clutter. It has been removed to improve code cleanliness and maintainability.

--- app/hooks/useRevalidate.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-import { useDataRefresh } from 'remix-utils';
-import emitter from 'tiny-emitter/instance';
-
-export function useRevalidate() {
-  const { refresh } = useDataRefresh();
-  useEffect(() => emitter.on('revalidate', () => refresh()), []);
-}
