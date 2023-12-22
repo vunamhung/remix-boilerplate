010625d434e8f760b9e8e7f061ca76b0199c5864
Hung Vu <vunamhung@gmail.com>
2023-12-22 16:25:00 +0700

    :recycle: Refactoring code.

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@ export default {
   postcss: true,
   tailwind: true,
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: [/^react-icons/],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
